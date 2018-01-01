e308dbc6c64172621a9b18fc867d194775b88eea
Kris Thompson <kt.devcat@gmail.com>
2018-01-01 19:03:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
   return driver;
 }
+
+function deleteFromDriverByKey(driver, key) {
+  newDriver = driver;
+  delete newDriver.key;
+  return newDriver;
+}
