18dae4f9b7a0c864b72a5a53904a78dcf7048654
Kris Thompson <kt.devcat@gmail.com>
2018-01-01 18:57:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   const newDriver = Object.assign({}, driver, {[key] : value});
   return newDriver;
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver.key = value;
+  return driver;
+}
